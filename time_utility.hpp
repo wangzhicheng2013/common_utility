86faa2ee3ec114911de8eb0de2e47e5d0949bca6
wangzhicheng <2363702560@qq.com>
2020-09-04 10:21:52 +0800

    增加函数get_cur_time

--- time_utility.hpp
+++ time_utility.hpp
@@ -15,6 +15,14 @@ public:
         strftime(now_time, sizeof(now_time), "%Y-%m-%d %H:%M:%S", ptm);
         date_str = now_time;
     }
+    inline std::string get_cur_time(const char *time_format = "%Y-%m-%d %T") {
+        struct timeval tv = { 0 };
+        gettimeofday(&tv, nullptr);
+        struct tm *ptm = localtime(&tv.tv_sec);
+        char now_time[64] = "";
+        strftime(now_time, sizeof(now_time), time_format, ptm);
+        return now_time;
+    }
 };
 
 #define  G_TIME_UTILITY single_instance<time_utility>::instance()
