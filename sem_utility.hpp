23d76b65aebc185c884e530bbfef4dea88f92b21
wangzhicheng <2363702560@qq.com>
2020-11-25 16:41:11 +0800

    增加sem工具

---
+++ sem_utility.hpp
@@ -0,0 +1,72 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <semaphore.h>
+#include <iostream>
+#include "single_instance.hpp"
+class sem_utility {
+public:
+    union semun {
+        int val = 0;
+        struct semid_ds *buf = nullptr;
+        unsigned short *array = nullptr;
+        struct seminfo *__buf = nullptr;
+    };
+    
+    bool init_sem(int sem_id, int val) {
+        union semun tmp;
+        tmp.val = val;
+        if (semctl(sem_id, 0, SETVAL, tmp)) {
+            return false;
+        }
+        return true;
+    }
+    int create_sem(key_t key, int val = 1) {
+        int sem_id = semget(key, 1, IPC_CREAT | 0666);
+        if (sem_id < 0) {
+            std::cerr << "semget for key:" << key << " failed." << std::endl;
+            return -1;
+        }
+        if (!init_sem(sem_id, val)) {
+            return -1;
+        }
+        return sem_id;
+    }
+    bool sem_p(int sem_id) {
+        struct sembuf sbuf;
+        sbuf.sem_num = 0;
+        sbuf.sem_op = -1;
+        sbuf.sem_flg = SEM_UNDO;
+
+        if (semop(sem_id, &sbuf, 1) < 0) {
+            std::cerr << "sem P failed." << std::endl;
+            return false;
+        }
+        return true;
+    }
+    bool sem_v(int sem_id) {
+        struct sembuf sbuf;
+        sbuf.sem_num = 0;
+        sbuf.sem_op = 1;
+        sbuf.sem_flg = SEM_UNDO;
+
+        if (semop(sem_id, &sbuf, 1) < 0) {
+            std::cerr << "sem V failed." << std::endl;
+            return false;
+        }
+        return true;
+    }
+    bool del_sem(int sem_id) {
+        union semun tmp;
+        if (semctl(sem_id, 0, IPC_RMID, tmp)) {
+            return false;
+        }
+        return true;
+    }
+};
+
+#define  G_SEM_UTILITY single_instance<sem_utility>::instance()
--- test.cpp
+++ test.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-#include "encry_utility.hpp"
+#include "string_utility.hpp"
 int main() {
-    std::string str;
-    G_ENCRY_UTILITY.make_md5_str("123", str);
-    std::cout << str << std::endl;
+    std::unordered_map<std::string, std::string>args;
+    const char *url = "http://www.123.com/jk/lll?key0=val0&key1=val1&key2=val2&keyXX0=val_0090*";
+    G_STRING_UTILITY.get_url_args(url, args);
+    for (auto &e : args) {
+        std::cout << e.first << " " << e.second << std::endl;
+    }
     
     return 0;
 }
