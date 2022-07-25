8220eb65a1f2da92fb1979d32e0abc7f085e77f7
wb7898 <wb7898@arcsoft.com.cn>
2022-07-25 16:28:54 +0800

    add blackening

--- file_utility.hpp
+++ file_utility.hpp
@@ -199,6 +199,67 @@ public:
         va_end(argp);
         printf("\n");
     }
+    // blacken the designated area of nv12 image
+    // left:Abscissa of upper left corner of rectangular area
+    // top:Vertical coordinate of upper left corner of rectangular area
+    // right:Abscissa of lower right corner of rectangular area
+    // bottom:Vertical coordinate of the lower right corner of the rectangular area
+    // width:Original image width
+    // height:Original image height
+    // ppu8Plane_y:Y channel address
+    // ppu8Plane_uv:UV channel address
+    void blackening_nv12(int left, 
+                        int top,
+                        int right, 
+                        int bottom,
+                        int width,
+                        int height,
+                        unsigned char *ppu8Plane_y,
+                        unsigned char *ppu8Plane_uv) {
+        if (!ppu8Plane_y || !ppu8Plane_uv) {
+            return;
+        }
+        if (!fix(left, top, right, bottom, width, height)) {
+            return;
+        }
+        int w = right - left;
+        for (int j = top;j <= bottom;j++) {
+            memset(ppu8Plane_y + left + j * width, 0, w);
+            memset(ppu8Plane_uv + left + j / 2 * width, 128, w);
+        }
+    }
+private:
+    // Correction of abscissa according to width
+    void fix_x(int &x, int width) {
+        if (x < 0) {
+            x = 0;
+        }
+        else if (x >= width) {
+            x = width - 1;
+        }
+    }
+    void fix_y(int &y, int height) {
+        if (y < 0) {
+            y = 0;
+        }
+        else if (y >= height) {
+            y = height - 1;
+        }    
+    }
+    // Correct rectangular area according to image width and height
+    bool fix(int &left, int &top, int &right, int &bottom, int width, int height) {
+        fix_x(left, width);
+        fix_x(right, width);
+
+        fix_y(top, height);
+        fix_y(bottom, height);
+        int w = right - left;
+        int h = bottom - top;
+        if (w <= 0 || h <= 0) {
+            return false;
+        }
+        return true;    
+    }
 };
 
 #define  G_FILE_UTILITY single_instance<file_utility>::instance()
