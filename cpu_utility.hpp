f06983f19df88586329fbbcecf0907ff6de0ef8b
wb7898 <wb7898@arcsoft.com.cn>
2024-03-08 08:55:34 +0800

    11

--- cpu_utility.hpp
+++ cpu_utility.hpp
@@ -152,6 +152,61 @@ public:
         }
         return -1;
     }
+    static int get_cpu_mhz(int cpu_no) {
+        char cmd[128] = { 0 };
+        char buf[256] = { 0 };
+        FILE *fp = nullptr;
+    #ifdef QNX_OS
+        // 8295 qnx cpu as following
+        /*
+        Processor1: 1091556528 Octa Kryo-695 Gold 2092MHz FPU
+        Processor2: 1091556528 Octa Kryo-695 Gold 2092MHz FPU
+        Processor3: 1091556528 Octa Kryo-695 Gold 2092MHz FPU
+        Processor4: 1091556528 Octa Kryo-695 Gold 2092MHz FPU
+        Processor5: 1091556544 Octa Kryo-695 Gold Plus 2380MHz FPU
+        Processor6: 1091556544 Octa Kryo-695 Gold Plus 2380MHz FPU
+        Processor7: 1091556544 Octa Kryo-695 Gold Plus 2380MHz FPU
+        Processor8: 1091556544 Octa Kryo-695 Gold Plus 2380MHz FPU
+        */
+        snprintf(cmd, sizeof(cmd), "pidin info | grep Processor%d | grep MHz", cpu_no + 1);
+        fp = popen(cmd, "r");
+        if (!fp) {
+            return 0;
+        }
+        fgets(buf, sizeof(buf) - 1, fp);
+        fclose(fp);
+        const char* ptr = strstr(buf, "MHz");
+        if (nullptr == ptr) {
+            return 0;
+        }
+        while (*ptr != ' ') {
+            if (ptr == buf) {
+                return 0;
+            }
+            --ptr;
+        }
+        return atoi(ptr + 1);
+    #elif defined(ANDROID_OS)
+        snprintf(cmd, sizeof(cmd), "cat /sys/devices/system/cpu/cpu%d/cpufreq/scaling_cur_freq", cpu_no);
+        fp = popen(cmd, "r");
+        if (!fp) {
+            return 0;
+        }
+        fgets(buf, sizeof(buf) - 1, fp);
+        fclose(fp);
+        return atoi(buf) / 1000;
+    #else
+        snprintf(cmd, sizeof(cmd), "cat /proc/cpuinfo | grep \"cpu MHz\" | sed -e 's/.*:[^0-9]//' | head -n %d | tail -n 1", cpu_no + 1);
+        fp = popen(cmd, "r");
+        if (!fp) {
+            return 0;
+        }
+        fgets(buf, sizeof(buf) - 1, fp);
+        fclose(fp);
+        return atoi(buf);   
+    #endif
+        return 0;
+    }
 };
 
 #define  G_CPU_UTILITY single_instance<cpu_utility>::instance()
